20e3f035d5420d47b935dc1841a4a36f7a697993
Ololade <olaonipekunololade94@gmail.com>
2023-03-26 14:46:58 +0100

    making more authentication modification

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -5,7 +6,17 @@ from django.urls import reverse
 
 def index(request):
     if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse("login"))
+        return HttpResponseRedirect(reverse("flightUsers:login"))
 
 def login_view(request):
-    return(request, "users/login.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("flightUser:index"))
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    return(request, "users/logout.html")
